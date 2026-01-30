2208035e00ccf166ba2b50bef7063596ae39475e
SergejMasich <sergejmasich74@gmail.com>
2026-01-30 19:55:02 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,1 +1,17 @@
+import ShieldLayer from "@/components/ShieldLayer";
+import HeroContent from "@/components/HeroContent";
 
+export default function Hero() {
+  return (
+    <section className="relative min-h-[100svh] w-full overflow-hidden">
+      {/* Layer 1: Shield + data streams background */}
+      <ShieldLayer />
+
+      {/* Layer 2: Foreground header/content */}
+      <HeroContent />
+
+      {/* Bottom fade to blend into next sections */}
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-40 bg-gradient-to-b from-transparent to-black" />
+    </section>
+  );
+}
