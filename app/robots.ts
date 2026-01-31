e50516b3e058a5ed09775b802b718b06e453391e
SergejMasich <sergejmasich74@gmail.com>
2026-01-31 18:16:53 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,22 +1,9 @@
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const base = "https://aspis-network-v3.vercel.app";
-
-  const now = new Date();
-
-  return [
-    {
-      url: `${base}/en`,
-      lastModified: now,
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: `${base}/ru`,
-      lastModified: now,
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-  ];
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://aspis-network-v3.vercel.app/sitemap.xml",
+    host: "https://aspis-network-v3.vercel.app",
+  };
 }
